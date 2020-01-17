7ab93f7c1a29ff68c88e3146d126871488345c3d
pqs <pengqiangsheng@leelen.cn>
2020-01-17 17:03:54 +0800

    更新

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -37,7 +37,7 @@ function Recommend (props) {
           <RecommendList recommendList={recommendListJS}></RecommendList> 
         </div>
       </Scroll>
-      { enterLoading ? <Loading></Loading> : null }
+      <Loading show={enterLoading}></Loading>
     </Content> 
   )
 }
